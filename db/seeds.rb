8233f763106495e9adc945cbd9ed39d5cfa9cda7
admande <adam.mandeville@gmail.com>
2016-01-17 12:08:12 -0500

    added charts

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+user = User.create :first_name => "John", :last_name => "G", :email => 'john@gmail.com', :password => 'topsecret', :password_confirmation => 'topsecret'
+
+Sleeplog.create :date => "2016-01-16", :hours => "6", :user_id => user.id
+Sleeplog.create :date => "2016-01-15", :hours => "3", :user_id => user.id
+Sleeplog.create :date => "2016-01-14", :hours => "7", :user_id => user.id
