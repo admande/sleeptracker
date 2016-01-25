56fa017df969dc549b3e0fcf924b9f34ad16001d
admande <adam.mandeville@gmail.com>
2016-01-24 21:58:35 -0500

    finished date validations

--- app/models/diary.rb
+++ app/models/diary.rb
@@ -1,5 +1,6 @@
 class Diary < ActiveRecord::Base
   before_validation :sentiment_analysis
+  validate :date_cannot_be_in_future
 
   belongs_to :user
 
@@ -8,6 +9,12 @@ class Diary < ActiveRecord::Base
   validates :body, presence: true
 
   private
+
+  def date_cannot_be_in_future
+    if date.present? && date > Date.today
+      errors.add(:date, "can't be in the future")
+    end
+  end
 
   def sentiment_analysis
     text = body
--- app/models/sleeplog.rb
+++ app/models/sleeplog.rb
@@ -1,8 +1,16 @@
 class Sleeplog < ActiveRecord::Base
   belongs_to :user
+  validate :date_cannot_be_in_future
 
   validates :user_id, presence: true, numericality: { only_integer: true }
   validates :date, presence: true, uniqueness: true
   validates :hours, presence: true, numericality: { greater_than: 0, less_than: 16 }
 
+  private
+
+  def date_cannot_be_in_future
+    if date.present? && date > Date.today
+      errors.add(:date, "can't be in the future")
+    end
+  end
 end
