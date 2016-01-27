b5947145cbfe0d4787538015cdec86a8583dc928
admande <adam.mandeville@gmail.com>
2016-01-27 10:59:22 -0500

    info page changes

--- app/controllers/diaries_controller.rb
+++ app/controllers/diaries_controller.rb
@@ -6,6 +6,12 @@ class DiariesController < ApplicationController
     @user = current_user
     if params[:diary_body_search]
       @diaries = Diary.search_by_diary_body(params[:diary_body_search]).order(:date)
+    elsif params[:order] == "sentiment"
+      @diaries = Diary.where(user: @user).order(sentiment: :desc)
+    elsif params[:order] == "reverse_sentiment"
+      @diaries = Diary.where(user: @user).order(sentiment: :asc)
+    elsif params[:order] == "reverse_date"
+      @diaries = Diary.where(user: @user).order(date: :asc)
     else
       @diaries = Diary.where(user: @user).order(date: :desc)
     end
@@ -24,7 +30,7 @@ class DiariesController < ApplicationController
     @diary.user = current_user
     if @diary.save
       flash[:notice] = "Diary added successfully"
-      redirect_to @diary
+      redirect_to '/diaries'
     else
       flash.now[:errors] = @diary.errors.full_messages.join(". ")
       render :new
